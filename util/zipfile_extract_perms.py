0f6a61e06c0d6c68bb78ae228a78fac353b5e216
Ariel Poliak <ariel.p@hostdime.com>
2016-09-21 00:00:22 -0400

    use proper inheritance rather than monkeypatching in permission extraction implementation

--- util/zipfile_extract_perms.py
+++ util/zipfile_extract_perms.py
@@ -1,6 +1,6 @@
 """
 NSAptr - a Non-Sketchy Android Platform Tools Retriever
-Monkey-patch built-in zipfile to include permissions extraction functionality
+Extend built-in zipfile.ZipFile to include permissions extraction functionality
 Implements part of the functionality described at:
   https://bugs.python.org/issue15795
   https://bugs.python.org/file34873/issue15795_cleaned.patch
@@ -30,96 +30,91 @@ For original code contained in this file, the following applies:
 """
 import os
 import shutil
-import zipfile
+from zipfile import *
 
-zipfile.__all__.extend(["PERMS_PRESERVE_NONE", 'PERMS_PRESERVE_SAFE', "PERMS_PRESERVE_ALL"])
-
-# Enum choices for Zipfile.extractall preserve_permissions argument
-zipfile.PERMS_PRESERVE_NONE, zipfile.PERMS_PRESERVE_SAFE, zipfile.PERMS_PRESERVE_ALL = range(3)
+# Enum choices for ZipFileMod.extractall preserve_permissions argument
 PERMS_PRESERVE_NONE, PERMS_PRESERVE_SAFE, PERMS_PRESERVE_ALL = range(3)
 
 
-def __extract(self, member, path=None, pwd=None,
-              preserve_permissions=zipfile.PERMS_PRESERVE_NONE):
-    """Extract a member from the archive to the current working directory,
-       using its full name. Its file information is extracted as accurately
-       as possible. `member' may be a filename or a ZipInfo object. You can
-       specify a different directory using `path'.
-    """
-    if not isinstance(member, zipfile.ZipInfo):
-        member = self.getinfo(member)
+class ZipFileMod(ZipFile):
+    def __init__(self, *args, **kwargs):
+        ZipFile.__init__(self, *args, **kwargs)
 
-    if path is None:
-        path = os.getcwd()
+    def _RealGetContents(self):
+        return super()._RealGetContents()
 
-    return self._extract_member(member, path, pwd, preserve_permissions)
+    def _extract_member(self, member, targetpath, pwd, preserve_permissions=PERMS_PRESERVE_NONE):
+        """Extract the ZipInfo object 'member' to a physical
+           file on the path targetpath.
+        """
+        # build the destination pathname, replacing
+        # forward slashes to platform specific separators.
+        arcname = member.filename.replace('/', os.path.sep)
 
+        if os.path.altsep:
+            arcname = arcname.replace(os.path.altsep, os.path.sep)
+        # interpret absolute pathname as relative, remove drive letter or
+        # UNC path, redundant separators, "." and ".." components.
+        arcname = os.path.splitdrive(arcname)[1]
+        invalid_path_parts = ('', os.path.curdir, os.path.pardir)
+        arcname = os.path.sep.join(x for x in arcname.split(os.path.sep)
+                                   if x not in invalid_path_parts)
+        if os.path.sep == '\\':
+            # filter illegal characters on Windows
+            arcname = self._sanitize_windows_name(arcname, os.path.sep)
 
-def __extractall(self, path=None, members=None, pwd=None,
-                 preserve_permissions=zipfile.PERMS_PRESERVE_NONE):
-    """Extract all members from the archive to the current working
-       directory. `path' specifies a different directory to extract to.
-       `members' is optional and must be a subset of the list returned by
-       namelist(). `preserve_permissions` controls whether permissions of
-       zipped files are preserved or not. Default is PERMS_PRESERVE_NONE -
-       do not preserve any permissions. Other options are to preserve safe
-       subset of permissions PERMS_PRESERVE_SAFE or all permissions
-       PERMS_PRESERVE_ALL.
-    """
-    if members is None:
-        members = self.namelist()
+        targetpath = os.path.join(targetpath, arcname)
+        targetpath = os.path.normpath(targetpath)
 
-    for zipinfo in members:
-        self.extract(zipinfo, path, pwd, preserve_permissions)
+        # Create all upper directories if necessary.
+        upperdirs = os.path.dirname(targetpath)
+        if upperdirs and not os.path.exists(upperdirs):
+            os.makedirs(upperdirs)
 
+        if member.filename[-1] == '/':
+            if not os.path.isdir(targetpath):
+                os.mkdir(targetpath)
+            return targetpath
 
-def ___extract_member(self, member, targetpath, pwd, preserve_permissions):
-    """Extract the ZipInfo object 'member' to a physical
-       file on the path targetpath.
-    """
-    # build the destination pathname, replacing
-    # forward slashes to platform specific separators.
-    arcname = member.filename.replace('/', os.path.sep)
+        with self.open(member, pwd=pwd) as source, \
+                open(targetpath, "wb") as target:
+            shutil.copyfileobj(source, target)
 
-    if os.path.altsep:
-        arcname = arcname.replace(os.path.altsep, os.path.sep)
-    # interpret absolute pathname as relative, remove drive letter or
-    # UNC path, redundant separators, "." and ".." components.
-    arcname = os.path.splitdrive(arcname)[1]
-    invalid_path_parts = ('', os.path.curdir, os.path.pardir)
-    arcname = os.path.sep.join(x for x in arcname.split(os.path.sep)
-                               if x not in invalid_path_parts)
-    if os.path.sep == '\\':
-        # filter illegal characters on Windows
-        arcname = self._sanitize_windows_name(arcname, os.path.sep)
+        if preserve_permissions in [PERMS_PRESERVE_SAFE, PERMS_PRESERVE_ALL]:
+            bitmask = 0x1FF if preserve_permissions == PERMS_PRESERVE_SAFE else 0xFFF
+            mode = member.external_attr >> 16 & bitmask
+            os.chmod(targetpath, mode)
 
-    targetpath = os.path.join(targetpath, arcname)
-    targetpath = os.path.normpath(targetpath)
-
-    # Create all upper directories if necessary.
-    upperdirs = os.path.dirname(targetpath)
-    if upperdirs and not os.path.exists(upperdirs):
-        os.makedirs(upperdirs)
-
-    if member.filename[-1] == '/':
-        if not os.path.isdir(targetpath):
-            os.mkdir(targetpath)
         return targetpath
 
-    with self.open(member, pwd=pwd) as source, \
-            open(targetpath, "wb") as target:
-        shutil.copyfileobj(source, target)
+    def extract(self, member, path=None, pwd=None,
+                preserve_permissions=PERMS_PRESERVE_NONE):
+        """Extract a member from the archive to the current working directory,
+           using its full name. Its file information is extracted as accurately
+           as possible. `member' may be a filename or a ZipInfo object. You can
+           specify a different directory using `path'.
+        """
+        if not isinstance(member, ZipInfo):
+            member = self.getinfo(member)
 
-    if preserve_permissions in [zipfile.PERMS_PRESERVE_SAFE, zipfile.PERMS_PRESERVE_ALL]:
-        bitmask = 0x1FF if preserve_permissions == zipfile.PERMS_PRESERVE_SAFE else 0xFFF
-        mode = member.external_attr >> 16 & bitmask
-        os.chmod(targetpath, mode)
+        if path is None:
+            path = os.getcwd()
 
-    return targetpath
+        return self._extract_member(member, path, pwd, preserve_permissions)
 
-zipfile.ZipFile.extract = __extract
-zipfile.ZipFile.extractall = __extractall
-zipfile.ZipFile._extract_member = ___extract_member
+    def extractall(self, path=None, members=None, pwd=None,
+                   preserve_permissions=PERMS_PRESERVE_NONE):
+        """Extract all members from the archive to the current working
+           directory. `path' specifies a different directory to extract to.
+           `members' is optional and must be a subset of the list returned by
+           namelist(). `preserve_permissions` controls whether permissions of
+           zipped files are preserved or not. Default is PERMS_PRESERVE_NONE -
+           do not preserve any permissions. Other options are to preserve safe
+           subset of permissions PERMS_PRESERVE_SAFE or all permissions
+           PERMS_PRESERVE_ALL.
+        """
+        if members is None:
+            members = self.namelist()
 
-# declare so we can import the modified class directly
-ZipFile = zipfile.ZipFile
+        for zipinfo in members:
+            self.extract(zipinfo, path, pwd, preserve_permissions)
